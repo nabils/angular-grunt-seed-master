53c48194dd933b659baa5d94eb4a5e20e03ed6af
Nabil Shuhaiber <nabil@shuhaiber.com>
2013-10-21 23:04:13 +0100

    Initial commit

---
+++ app/js/controllers.js
@@ -0,0 +1,21 @@
+'use strict';
+
+/* Controllers */
+
+
+// function MyCtrl1() {}
+// MyCtrl1.$inject = [];
+
+
+// function MyCtrl2() {
+// }
+// MyCtrl2.$inject = [];
+
+angular.module('myApp.controllers', []).
+	controller('MyCtrl1', function() {
+
+	}).
+	
+	controller('MyCtrl2', function() {
+
+	});
